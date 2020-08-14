782796446ce0276a1ff99e8feeb1cd71e2a4ee1a
Austin T Leon <Austin1995@comcast.net>
2020-08-14 10:49:13 -0700

    Update Card_game.py

--- Card_game.py
+++ Card_game.py
@@ -3,6 +3,7 @@ class Player():
 		name = ''
 		hand = []
 	def changeName(self, name):
+	"""This method allows you to change the name of select player"""
 		self.name = name
 
 class Game():
@@ -16,11 +17,13 @@ class Game():
 		self.gameType = gameType
 
 	def addPlayers(self):
+	"""This method adds a player to the currentPlayers list and adds 1 to the numOfPlayers of the game"""
 		Game.numOfPlayers += 1
 		self.currentPlayers.append('Player: {}'.format(str(Game.numOfPlayers)))
 
 
 	def showPlayers(self):
+	"""Prints all the players in game"""
 		print(self.currentPlayers)
 
 class Poker(Game):
