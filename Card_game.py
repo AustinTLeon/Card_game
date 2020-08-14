91dadc184a68d7904d88dd7c59c4435fc747b0a3
Austin T Leon <Austin1995@comcast.net>
2020-08-14 10:46:09 -0700

    Update Card_game.py

--- Card_game.py
+++ Card_game.py
@@ -1,27 +1,27 @@
-class Game():
-	name = ''
-	game_type = ''
-	current_players = {}
-
-	def __init__(self, name, game_type):
-		
-		self.name = name
-		self.game_type = game_type
-
-	def add_players(self, player):
-		self.current_players[player] = player.hand
-		
 class Player():
-	name = ''
-	hand = []
-	def __init__(self, name):
+	def __init__(self):
+		name = ''
+		hand = []
+	def changeName(self, name):
 		self.name = name
+
+class Game():
+
+	currentPlayers = [Player()]
+	numOfPlayers = 0
+
+	def __init__(self, name, gameType):
+		
+		self.name = name
+		self.gameType = gameType
+
+	def addPlayers(self):
+		Game.numOfPlayers += 1
+		self.currentPlayers.append('Player: {}'.format(str(Game.numOfPlayers)))
+
+
+	def showPlayers(self):
+		print(self.currentPlayers)
 
 class Poker(Game):
 	max_hand_size = 5
-
-
-
-
-
-
